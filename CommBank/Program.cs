6ae2528c08d7413665063e3caeab269508e5cd82
mramotar <mramotar@dropbox.com>
2022-06-09 21:20:22 -0400

    Fix connection

--- CommBank/Program.cs
+++ CommBank/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.Configure<DatabaseSettings>(
-    builder.Configuration.GetSection("CommBank")
+    builder.Configuration.GetSection("CommBankDatabase")
 );
 
 builder.Services.AddSingleton<ApplicationsService>();
